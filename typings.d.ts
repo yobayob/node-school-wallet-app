2e66f60228166a4f06279ca96073085ef36d0f89
e.konechnyi <yobayob@yandex.ru>
2017-10-05 14:19:56 +0300

    fix typings

--- typings.d.ts
+++ typings.d.ts
@@ -7,3 +7,17 @@ declare module 'json!*' {
 	const value: any;
 	export default value;
 }
+
+declare module 'card-info' {
+	export class CardInfo {
+		balance: number;
+		bankName: string;
+		backgroundColor: string;
+		textColor: string;
+		bankLogoSvg: string;
+		brandLogoSvg: string;
+		bankAlias: string;
+		numberNice: number;
+		constructor(number: string, o: any)
+	}
+}
